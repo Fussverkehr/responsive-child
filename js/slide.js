64e4ac23d103d9605766a9c918b4c91c05426cf6
Fussverkehr <dominik.bucheli@fussverkehr.ch>
2019-06-19 09:19:57 +0200

    Update slide.js
    JQuery safe mode

--- js/slide.js
+++ js/slide.js
@@ -1,43 +1,43 @@
-jQuery(function(){
+jQuery(document).ready(function($){
 
-				jQuery('.slideshow').wrap('<div class="slideshowcontainer"></div>');
-				jQuery('ul.slideshow a').hide().eq(0).show().addClass('active');			
-				jQuery('.slideshowcontainer').after('<a href="#" id="backbutton"><< </a><a href="#" id="forbutton">>></a>');
-				jQuery('ul.slideshow a span').hide();
-				var myTimer = setInterval(function startAni(){jQuery('#forbutton').trigger('click');		
+				$('.slideshow').wrap('<div class="slideshowcontainer"></div>');
+				$('ul.slideshow a').hide().eq(0).show().addClass('active');			
+				$('.slideshowcontainer').after('<a href="#" id="backbutton"><< </a><a href="#" id="forbutton">>></a>');
+				$('ul.slideshow a span').hide();
+				var myTimer = setInterval(function startAni(){$('#forbutton').trigger('click');		
 					    },7000);
 				$('.slideshowcontainer').hover(function(){
 				    clearInterval(myTimer);
 				    $('ul.slideshow a span').fadeIn(1000);
 				    			},
 				function(){
-				    jQuery('ul.slideshow a span').fadeOut(1000);
-				    var myTimer = setInterval(function startAni(){jQuery('#forbutton').trigger('click');		
+				    $('ul.slideshow a span').fadeOut(1000);
+				    var myTimer = setInterval(function startAni(){$('#forbutton').trigger('click');		
 				    	    },5000);				    
 				});	    				
 				
-				jQuery('#forbutton').on('click' , function(){
-				var currentImage = jQuery('ul.slideshow a.active');
+				$('#forbutton').on('click' , function(){
+				var currentImage = $('ul.slideshow a.active');
 				
-					if (jQuery('ul.slideshow a.active').next().length == 0){
-					jQuery('ul.slideshow a.active').fadeOut(3000).removeClass('active');
-					jQuery('ul.slideshow a:first-child').fadeIn(3000).addClass('active');
+					if ($('ul.slideshow a.active').next().length == 0){
+					$('ul.slideshow a.active').fadeOut(3000).removeClass('active');
+					$('ul.slideshow a:first-child').fadeIn(3000).addClass('active');
 					} else{
-				jQuery('ul.slideshow a.active').fadeOut(3000).removeClass('active').next().fadeIn(3000).addClass('active');
+				$('ul.slideshow a.active').fadeOut(3000).removeClass('active').next().fadeIn(3000).addClass('active');
 						}
 				return false;
 			});
 	 
-jQuery('#backbutton').on( 'click' , function(){
-	var currentImage = jQuery('ul.slideshow a.active');
+$('#backbutton').on( 'click' , function(){
+	var currentImage = $('ul.slideshow a.active');
 	
-		if (jQuery('ul.slideshow a.active').prev().length == 0){
+		if ($('ul.slideshow a.active').prev().length == 0){
 		
-		jQuery('ul.slideshow a:last-child').fadeOut(3000).removeClass('active');
+		$('ul.slideshow a:last-child').fadeOut(3000).removeClass('active');
 		
-		jQuery('ul.slideshow a:last-child').fadeIn(3000).addClass('active');
+		$('ul.slideshow a:last-child').fadeIn(3000).addClass('active');
 		} else{
-	jQuery('ul.slideshow a.active').fadeOut(3000).removeClass('active').prev().fadeIn(3000).addClass('active');
+	$('ul.slideshow a.active').fadeOut(3000).removeClass('active').prev().fadeIn(3000).addClass('active');
 			}
 	return false;
 });
