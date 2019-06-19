c33241e987746b235bf1865983fa2c2058de87de
Fussverkehr <dominik.bucheli@fussverkehr.ch>
2019-06-19 10:53:23 +0200

    Revert "Update slide.js"
    This reverts commit 1601fdeca9a0d3ec7414f02cc18f3a66154766d5.

--- js/slide.js
+++ js/slide.js
@@ -6,14 +6,14 @@
 				jQuery('ul.slideshow a span').hide();
 				var myTimer = setInterval(function startAni(){jQuery('#forbutton').trigger('click');		
 					    },7000);
-				jQuery('.slideshowcontainer').on("mouseover",function(){
-				  clearInterval(myTimer);
+				jQuery('.slideshowcontainer').hover(function(){
+				    clearInterval(myTimer);
 				    jQuery('ul.slideshow a span').fadeIn(1000);
 				    			},
-				jQuery('.slideshowcontainer').on("mouseout",function(){
+				function(){
 				    jQuery('ul.slideshow a span').fadeOut(1000);
 				    var myTimer = setInterval(function startAni(){jQuery('#forbutton').trigger('click');		
-				    	    },7000);				    
+				    	    },5000);				    
 				});	    				
 				
 				jQuery('#forbutton').on('click' , function(){
